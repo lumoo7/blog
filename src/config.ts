94861cced7721f7ab232770a4fb4a5e41c5a6cad
huangsnw <huangsnw@outlook.com>
2024-04-29 14:18:34 +0800

    update config

--- src/config.ts
+++ src/config.ts
@@ -7,21 +7,22 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',
+  title: 'LuMoo',
+  subtitle: 'lumoo',
+  lang: 'zh_CN',
   themeHue: 250,
   banner: {
     enable: false,
     src: 'assets/images/demo-banner.png',
   },
-  favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
-  ]
+  favicon: [
+    // Leave this array empty to use the default favicon
+    {
+      src: '/favicon/icon.png', // Path of the favicon, relative to the /public directory
+      theme: 'light', // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      sizes: '32x32', // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    },
+  ],
 }
 
 export const navBarConfig: NavBarConfig = {
@@ -29,17 +30,17 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',
-      external: true,
-    },
+    // {
+    //   name: 'GitHub',
+    //   url: 'https://github.com/saicaca/fuwari',
+    //   external: true,
+    // },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',
-  name: 'Lorem Ipsum',
+  name: 'Lu Moo',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
